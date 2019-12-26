e509d510a7430b4069406626e51fcc82ba7f90d0
Danial Kurkovskiy <dan_9494@mail.ru>
2019-12-26 18:13:37 +0300

    Начало выполнения Практического задания №6

--- src/Task6.java
+++ src/Task6.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Task6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("X=");
+        int x = scan.nextInt();
+        System.out.println("Y=");
+        int y = scan.nextInt();
+        System.out.println("Z=");
+        int z = scan.nextInt();
+        int sum = (x + y + z)/3;
+        System.out.print("Sum: " + sum);
+    }
 }

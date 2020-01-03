df689929b4e4b41c3fabe2f24913a0f258dc2a22
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-04 00:31:10 +0300

    Выполнение практического задания №7

--- src/Task7.java
+++ src/Task7.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Task7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Number = ");
+        int number = scan.nextInt();
+        int X = 5;
+        int Y = 10;
+        int Z = 15;
+        if (number == X)
+            System.out.println("Данное значение имеется в константах");
+        else if (number == Y)
+            System.out.println("Данное значение имеется в константах");
+        else if (number == Z)
+            System.out.println("Данное значение имеется в константах");
+        else System.out.println("Такой константы нет!");
+    }
 }

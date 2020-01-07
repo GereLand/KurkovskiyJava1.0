2fdbb4d97fc0dd6def575b26898a26f17cb5444d
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-08 01:39:13 +0300

    Выполнение практического задания №10

--- src/Task10.java
+++ src/Task10.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Task10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Количество строк матрицы = ");
+        int x = scan.nextInt();
+        System.out.print("Количество столбцов матрицы = ");
+        int y = scan.nextInt();
+        int[][] massiv = new int[x][y];
+        int c = 1;
+        int e = 1;
+        for (int a = 0; a < x; a++) {
+            for (int b = 0; b < y; b++) {
+                {
+                    System.out.print("Ячейка " + c + "." + e + " = ");
+                    massiv [a][b] = scan.nextInt();
+                }
+                e++;
+            }
+            e = 1;
+            c++;
+        }
+        int d = 0;
+        for (int a = 0; a < x; a++) {
+            System.out.println(massiv[a][d] * 3);
+        }
+    }
 }

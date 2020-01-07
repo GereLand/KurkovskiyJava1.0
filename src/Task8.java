57e7514bb9c210eabb4afd609fbd79549965a9e1
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-07 15:36:54 +0300

    Выполнение практического задания №8

--- src/Task8.java
+++ src/Task8.java
@@ -1,2 +1,9 @@
 public class Task8 {
+    public static void main(String[] args) {
+        int x = 1;
+        while(x <= 50) {
+            System.out.println(x);
+            x++;
+        }
+    }
 }

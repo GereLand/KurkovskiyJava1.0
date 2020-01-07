0ab3353652471eb091af7cad9a0bde97d9042dd0
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-07 18:43:19 +0300

    Выполнение практического задания №9

--- src/Task9.java
+++ src/Task9.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Task9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Размер массива = ");
+        int x = scan.nextInt();
+        int[] mas = new int[x];
+        for (int y = 0; y < x;) {
+            System.out.print("Массив 1 = ");
+            mas[y] = scan.nextInt();
+            y++;
+        }
+        for (int m: mas) {
+            System.out.println(m*2);
+        }
+    }
 }

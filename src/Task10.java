bf68301c768bc492d10970a211d19bce026cbcbc
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-09 14:19:25 +0300

    Оптимизация

--- src/Task10.java
+++ src/Task10.java
@@ -8,18 +8,13 @@ public class Task10 {
         System.out.print("Введите количество столбцов матрицы = ");
         int y = scan.nextInt();
         int[][] array = new int[x][y];
-        int c = 1;
-        int e = 1;
         for (int a = 0; a < x; a++) {
             for (int b = 0; b < y; b++) {
                 {
-                    System.out.print("Введите данные в ячейку " + c + "." + e + " = ");
+                    System.out.print("Введите данные в ячейку " + (a+1) + "." + (b+1) + " = ");
                     array[a][b] = scan.nextInt();
                 }
-                e++;
             }
-            e = 1;
-            c++;
         }
         for (int j = 0; j < y; j++) {
             System.out.println(array[0][j] * 3);

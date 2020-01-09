7415bd055e4a575a4bdf02369bc1eedf0ddfc511
Danial Kurkovskiy <dan_9494@mail.ru>
2020-01-09 16:53:38 +0300

    Выполнение практического задания №11

---
+++ src/Task11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите текст строки: ");
+        String string1 = scan.nextLine();
+        System.out.println(string1);
+    }
+}

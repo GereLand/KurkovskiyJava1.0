20059d3016bdf1982b41d63e79445d85b5604456
GereLand <dan_9494@mail.ru>
2020-01-23 21:39:43 +0300

    Перенос проекта для удобства личного пользования

--- src/Draft/DebtCounter.java
+++ src/Draft/DebtCounter.java
@@ -1,7 +1,9 @@
+package Draft;
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class DebtСounter {
+class DebtСounter {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите сумму долга: ");
@@ -10,8 +12,9 @@ public class DebtСounter {
             double procent = credit * 100 / 50000;
             String commission = new DecimalFormat("#0.00").format(((procent - 100)/100) * (credit - 50000));
             System.out.println("Сумма комиссии = " + commission + "₽");
-            String dolg = new DecimalFormat("#0.00").format(credit + commission);
-            System.out.print("Сумма возврата = " + dolg + "₽");
+            String dolg = credit + commission;
+            String dolg2 = new DecimalFormat("#0.00").format(dolg);
+            System.out.print("Сумма возврата = " + dolg2 + "₽");
         } else {
             System.out.print("Сумма долга не превышает лимит.");
         }

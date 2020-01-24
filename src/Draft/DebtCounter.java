843613c3674a5bd4086a6f63cb448249c4c7378c
GereLand <dan_9494@mail.ru>
2020-01-24 13:55:32 +0300

    Всё таки получилось сделать работоспособный долговой калькулятор

--- src/Draft/DebtCounter.java
+++ src/Draft/DebtCounter.java
@@ -8,15 +8,16 @@ class DebtСounter {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите сумму долга: ");
         double credit = in.nextDouble();
-        if (credit > 100) {
+        if (credit > 50000) {
             double procent = credit * 100 / 50000;
-            String commission = new DecimalFormat("#0.00").format(((procent - 100)/100) * (credit - 50000));
-            System.out.println("Сумма комиссии = " + commission + "₽");
-            String dolg = credit + commission;
-            String dolg2 = new DecimalFormat("#0.00").format(dolg);
-            System.out.print("Сумма возврата = " + dolg2 + "₽");
+            double commission = ((procent - 100)/100) * (credit - 50000);
+            double dolg = credit + commission;
+            String x = new DecimalFormat("#0.00").format(commission);
+            String y = new DecimalFormat("#0.00").format(dolg);
+            System.out.println("Сумма комиссии = " + x + "₽");
+            System.out.print("Сумма возврата = " + y + "₽");
         } else {
-            System.out.print("Сумма долга не превышает лимит.");
+            System.out.print("Сумма долга не превышает лимит!");
         }
     }
 }
